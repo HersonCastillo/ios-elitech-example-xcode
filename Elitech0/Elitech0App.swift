4069fbd479b55b3642e437e506fe05cbb37c61cb
HC <HC>
2021-11-13 21:38:32 -0600

    Initial Commit

---
+++ Elitech0/Elitech0App.swift
@@ -0,0 +1,17 @@
+//
+//  Elitech0App.swift
+//  Elitech0
+//
+//  Created by Herson Castillo on 13/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Elitech0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
